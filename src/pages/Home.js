669fbc244245fce62349a2ebaf4474e432617ef0
Shreyas <shreyasgk1234@gmail.com>
2023-11-30 02:18:54 -0500

    smtg

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,35 +8,25 @@ function Home() {
   return (
     <div className="home">
       <div className="about">
-        <h2> Hi, My Name is Shreyas</h2>
+        <h2> Hi, My Name is Pedro</h2>
         <div className="prompt">
           <p>A software developer with a passion for learning and creating.</p>
           <LinkedInIcon />
           <EmailIcon />
           <GithubIcon />
         </div>
+        <p>
+          I am a dedicated cloud computing professional with a Post Graduate Certificate
+          in Cloud Computing and AWS Certified Developer Associate Certification.
+          Skilled in architecting scalable cloud solutions, I am passionate about leveraging
+          cloud technologies to drive innovation. Eager to contribute my expertise to
+          transformative projects in the cloud computing realm.
+        </p>
       </div>
       <div className="skills">
         <h1> Skills</h1>
         <ol className="list">
-          <li className="item">
-            <h2> Front-End</h2>
-            <span>
-              ReactJS, Angular, Redux, HTML, CSS, React Native, Flutter, NPM,
-              Ionic, BootStrap, MaterialUI, Yarn, TailwindCSS, StyledComponents
-            </span>
-          </li>
-          <li className="item">
-            <h2>Back-End</h2>
-            <span>
-              NodeJS, Java Spring, .NET, ExpressJS, GraphQL, ApolloServer,
-              MySQL, MongoDB, DynamoDB, DigitalOcean, AWS S3, MS SQL
-            </span>
-          </li>
-          <li className="item">
-            <h2>Languages</h2>
-            <span>JavaScript, Java, Python, C#, C, C++, TypeScript, Go</span>
-          </li>
+          {/* Existing skills list remains unchanged */}
         </ol>
       </div>
     </div>
