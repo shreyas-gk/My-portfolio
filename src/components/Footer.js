6abfe4af464bf3d72880d273b2136f5625f81291
Shreyas <shreyasgk1234@gmail.com>
2023-11-30 02:32:17 -0500

    smtg

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
         <FacebookIcon />
         <LinkedInIcon />
       </div>
-      <p> &copy; 2022 pedrotech.com</p>
+      <p> &copy; 2023 Shreyas G Koundinya</p>
     </div>
   );
 }
